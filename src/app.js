4aa73cad796738790be22b0317c5aee9b92d70e7
FrauY <yuliayulina55omsk@gmail.com>
2023-03-12 20:59:02 +0100

    Api integrated

--- src/app.js
+++ src/app.js
@@ -1,1 +1,3 @@
-
+let apiKey = "9eddb9ab087ae2f5e27fac758d4297e4";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid={9eddb9ab087ae2f5e27fac758d4297e4}`;
+console.log(apiUrl);
