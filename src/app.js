c98daf16752e0327cc665da724ef2638b432a8c4
FrauY <yuliayulina55omsk@gmail.com>
2023-03-19 16:55:46 +0100

    Showing current data

--- src/app.js
+++ src/app.js
@@ -1,3 +1,19 @@
-let apiKey = "9eddb9ab087ae2f5e27fac758d4297e4";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid={9eddb9ab087ae2f5e27fac758d4297e4}`;
+function displayTemperature(response) {
+  console.log(response);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Moscow&appid=${apiKey}&units=metric`;
 console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemperature);
